920802f2cea5c0d5afd4d8f766792ced93ba5bc1
abdulprogramer <125268990+abdulprogramer@users.noreply.github.com>
2023-04-03 00:36:11 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,170 @@
+//1 write a program to find maximum between two numbers
+
+        var A=20
+        var B=10
+     if(A>B)
+        console.log("your entered number is A is grater than B")
+     else
+        console.log("your entered number is B is grater than A")
+
+//2 write a program to find maximum between three numbers
+        var A=5
+        var B=10
+        var C=15
+    if(A<B)
+        console.log("var A is lessthan B")
+    else if(B<C)
+        console.log("var B is lessthan C ")
+    else
+        console.log("invalid statmant")
+    
+    
+//3 write a program to check number positive negative and zero
+        var A=-100
+    if(A<0)
+        console.log("your enter number is negative")
+    else if(A>0)
+        console.log("your enter number is positive")
+    else
+        console.log("your enter number is zero")
+
+// 4 write a program to show tht the nimber is devisable on 5 and 11
+
+         var A=11
+
+    if(A%5==0)
+        console.log("Your entered number is devisable on 5")
+    else if(A%11==0)   
+        console.log("Your entered number is devisable on 11")
+    else
+        console.log("Your entered number is not devisable on 5 and 11")
+// 5 write a program thatchek the number is even or odd
+        var A=19
+    if(A%2==0)
+        console.log("Your entered number is even")
+    else
+        console.log("Your entered number is odd")
+// 6 write a program and check the year is leap year or not
+        var A=2023
+    if(A%4==0)
+        console.log("your entered year is leap year")
+    else
+        console.log("your entered year is not a leap year")
+
+//7 write a program to check the chracter is alphabet or not
+    var abc="a"
+    if(abc>="a" && abc<="z")
+    console.log("Your number is alphabet")
+    else
+    console.log("Your number is not alphabet")
+
+// 8 write a program that display the number is vowle or not
+        var vowl="a"
+    if(vowl=="a")
+         console.log("your entered word is vowl")
+    else if(vowl=="e")
+        console.log("your entered word is vowl")
+    else if(vowl=="i")
+         console.log("your entered word is vowl")
+    else if(vowl=="o")
+        console.log("your entered word is vowl")
+    else if(vowl=="u")
+        console.log("your entered word is vowl")
+    else if(vowl=="A")
+        console.log("your entered word is vowl")
+    else if(vowl=="E")
+       console.log("your entered word is vowl")
+    else if(vowl=="I")
+        console.log("your entered word is vowl")
+    else if(vowl=="O")
+       console.log("your entered word is vowl")
+    else if(vowl=="U")
+       console.log("your entered word is vowl")
+    else
+        console.log("your entered number is not a vowl")
+        
+
+  
+  
+  // 9 write a program to input any chracter and check it is alphabet chracter or special chracter
+
+   var def="1000"
+    if(def>="a" && def<="z")
+    console.log("Your number is alphabet")
+    else if(def>0 )
+    console.log("Your number a digit")
+    else 
+    console.log("You enter a spcial chracter")
+
+
+    //10 write a program to check the chracter is uppercase and lowercase
+
+    var abc="z"
+    if(abc>="A" && abc<="Z")
+    console.log("your number is uppercase")
+    else
+    console.log("your number is lowercase")
+
+    //11 write a program that input weak number and display day name
+    var day = 6
+    if(day==1)
+    console.log("Monday")
+    else if(day==2)
+    console.log("Tuesday")
+    else if(day==3)
+    console.log("Wednesday")
+    else if(day==4)
+    console.log("Thursday")
+    else if(day==5)
+    console.log("Friday")
+    else if(day==6)
+    console.log("Saturday")
+    else if(day==7)
+    console.log("Sunday")
+    else 
+    console.log("Invalid statment")
+    //14 write a program to check the sides of triangle or not
+    var a=45
+    var b=45
+    var c=45
+    if(a==45&&b==45&&c==45)
+    console.log("These sides of are make a Triangle")
+    else
+    console.log("It is not a triangle")
+
+    // 18 write a program to check the frofit or loss
+
+    var AmountBeforeDeal=10000
+    var AmountAfterDeal=9000
+    if(AmountBeforeDeal<AmountAfterDeal)
+    console.log("You get a profit in this deal")
+    else
+    console.log("You get a loss in this deal")
+    // 19 write a program that input the marks of 5 subjects
+    var Bio=40
+    var Math=30
+    var Physics=35
+    var Chemistery=25
+    var Computer=35
+    obtainmarks= Physics + Bio + Chemistery + Computer+ Math
+     marks=obtainmarks/250*100
+    if(marks<=90)
+    console.log("Grade is A")
+
+    // write a program to enter input month number of day in that month
+
+    var month=2
+    if(month==1||month==3||month==5||month==7||month==8||month==10||month==12)
+    console.log("the entered month have 31 days")
+    else if(month==4||month==6||month==9||month==11)
+    console.log("the entered month have 30 days")
+    else if(month==2)
+    console.log("your entered month have 28,29 days")
+    else
+    console.log("Invalid statment")
+
+    // 21 write a program that input bill of electercity
+    var unit =173
+    if(unit<=50)
+    totalunit=unit*0.50
+    console.log("Your bill is %d",totalunit)
